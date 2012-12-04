c7aa44f1e073f464e03790b5fa4c86d33b4919c0
root <root@froggy.(none)>
2012-12-04 19:51:24 +0100

    Initial commit

---
+++ lib/command.rb
@@ -0,0 +1,71 @@
+class DisplayRedirection < Vagrant::Command::Base
+  def execute
+    @vagrant_env=Vagrant::Environment.new
+    options = {}
+    opts = OptionParser.new do |opts|
+        opts.banner = "Enters sandbox state"
+        opts.separator ""
+        opts.separator "Usage: vagrant sandbox on <vmname>"
+    end
+    argv = parse_options(opts)
+    return if !argv
+    boxs=argv[0]
+    get_redirection(boxs)
+  end
+  
+  def command(command,options = {})
+      STDOUT.sync = true
+      output=nil
+      result=IO.popen("#{command}") {|f| output=f.readlines}
+      return output
+    end
+
+  def get_redirection(selection)
+     on_selected_vms(selection) do |boxname|
+        instance_uuid="#{@vagrant_env.vms[boxname.to_sym].uuid}"
+        results=command("VBoxManage showvminfo #{instance_uuid} |grep \"^NIC [0-9] Rule\" | sed 's/^.*host port = \\([0-9]*\\).*guest port = \\([0-9]*\\)/\\1 => \\2/g' ")
+        puts "[#{boxname}] - Redirect : "
+     	results.each { |result| puts "#{result}" }
+     end
+
+  end
+
+    def is_vm_created?(boxname)
+      if @vagrant_env.vms[boxname.to_sym].nil?
+        puts "[#{boxname}] - Box name doesn't exist in the Vagrantfile"
+        return false
+      else
+        return !@vagrant_env.vms[boxname.to_sym].uuid.nil?
+      end
+    end
+    
+  def on_selected_vms(selection,&block)
+        if selection.nil?
+          @vagrant_env.vms.each do |name,vm|
+            if @vagrant_env.multivm?
+              if name.to_s!="default"
+                if is_vm_created?(name)
+                  yield name
+                else
+                  puts "[#{name}] - machine not created1"
+                end
+              end
+            else
+              if is_vm_created?(name)
+                yield name
+              else
+                puts "[#{name}] - machine not created"
+              end
+            end
+          end
+        else
+          if is_vm_created?(selection)
+            yield selection
+          else
+            puts "[#{selection}] - machine not created"
+          end
+        end
+      end
+  end
+
+Vagrant.commands.register(:redirection) { DisplayRedirection }
---
+++ vagrant-getredirection.gemspec
@@ -0,0 +1,11 @@
+Gem::Specification.new do |s|
+  s.name = %q{vagrant-getredirection}
+  s.version = "0.0.1"
+  s.date = %q{2012-12-04}
+  s.authors = ["Loïc PORTE"]
+  s.email = %q{loic.porte@bibabox.fr}
+  s.summary = %q{vagrant-getredirection, tool to get boxes redirections.}
+  s.homepage = %q{https://github.com/garrettux/knife-vagrant}
+  s.description = %q{vagrant-getredirection, tool to get boxes redirections.}
+  s.files = ["lib/command.rb","lib/vagrant_init.rb"]
+end
